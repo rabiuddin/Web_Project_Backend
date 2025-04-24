a6775a119c1759b6e30623a8e485354de8bf945a
khizr80 <khizr80@gmail.com>
2025-04-24 19:35:38 +0500

    auth middleware done

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -13,6 +13,7 @@ export const verifyJWT = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded; // Store user info for use in controllers
+    // console.info("Token verified successfully:", decoded);
     next();
   } catch (err) {
     throw new ApiError(401, "Invalid or expired token");
