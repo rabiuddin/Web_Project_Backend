e173e9d7bf60737232d80fa05e3b1cce793a475c
Muhammad Rabi Uddin <rabiuddin1@gmail.com>
2025-04-30 17:31:20 +0500

    feat: add file model with schema for user files

---
+++ src/models/file.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "./user.model";
+
+const fileSchema = mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    name: {
+      type: String,
+      default: "New File",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    Timestamp: true,
+  }
+);
+
+export const File = mongoose.model("File", fileSchema);
