0e051cd7293bf547b6e6245a69262cc47889fb79
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2025-05-10 00:28:27 +0500

    feat: enhance code execution and analysis features with language support and complexity calculations

--- src/routes/code.routes.js
+++ src/routes/code.routes.js
@@ -1,10 +1,10 @@
-import { executeFile,analyzeFile } from "../controllers/code.controllers.js";
+import { executeCode, analyzeCode } from "../controllers/code.controllers.js";
 import express from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/execute/:id", verifyJWT, executeFile);
-router.post("/analyze/:id", verifyJWT, analyzeFile);
+router.post("/execute", verifyJWT, executeCode);
+router.post("/analyze", verifyJWT, analyzeCode);
 
 export default router;
